29ed7ce31c89de0ec3da5311690afec6d15ac2db
Lucas Miranda <lucas-m@outlook.com>
2021-02-28 16:23:59 -0300

    feat: add transaction module

--- lib/rocketpay/accounts/deposit.ex
+++ lib/rocketpay/accounts/deposit.ex
@@ -10,11 +10,8 @@ defmodule Rocketpay.Accounts.Deposit do
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
-      {:error, _operation, reason, _changes} ->
-        {:error, reason}
-
-      {:ok, %{deposit: account}} ->
-        {:ok, account}
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{deposit: account}} -> {:ok, account}
     end
   end
 end
